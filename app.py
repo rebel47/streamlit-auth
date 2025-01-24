20247537a3856bc13a9cde470b616816ac1db2ac
Mohammad Ayaz Alam <48854533+rebel47@users.noreply.github.com>
2025-01-25 00:09:25 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ from streamlit_authenticator.utilities import (CredentialsError,
 with open('config.yaml', 'r', encoding='utf-8') as file:
     config = yaml.load(file, Loader=SafeLoader)
 
-# Display logo and version
+st.image('logo.png')
+
 st.metric('Version', '0.4.1')
 
-# Display credentials (for debugging purposes)
 st.code(f"""
 Credentials:
 
@@ -41,46 +41,57 @@ authenticator = stauth.Authenticate(
     config['cookie']['expiry_days']
 )
 
+# authenticator = stauth.Authenticate(
+#     '../config.yaml'
+# )
+
 # Creating a login widget
 try:
-    name, authentication_status, username = authenticator.login('Login', 'main')
+    authenticator.login()
 except LoginError as e:
     st.error(e)
 
-# Check authentication status
-if st.session_state.get("authentication_status"):
+if st.session_state["authentication_status"]:
     st.write('___')
-    authenticator.logout('Logout', 'main')
+    authenticator.logout()
     st.write(f'Welcome *{st.session_state["name"]}*')
-    st.title('Some content')
+    st.title('Some content')    
     st.write('___')
-elif st.session_state.get("authentication_status") is False:
+elif st.session_state["authentication_status"] is False:
     st.error('Username/password is incorrect')
-elif st.session_state.get("authentication_status") is None:
+elif st.session_state["authentication_status"] is None:
     st.warning('Please enter your username and password')
 
+st.subheader('Guest login')
+
+# Creating a guest login button
+try:
+    authenticator.experimental_guest_login('Login with Google', provider='google',
+                                            oauth2=st.secrets["oauth2"])
+    authenticator.experimental_guest_login('Login with Microsoft', provider='microsoft',
+                                            oauth2=st.secrets["oauth2"])
+except LoginError as e:
+    st.error(e)
+
 # Creating a password reset widget
-if st.session_state.get("authentication_status"):
+if st.session_state["authentication_status"]:
     try:
-        if authenticator.reset_password(username):
+        if authenticator.reset_password(st.session_state["username"]):
             st.success('Password modified successfully')
-            with open('config.yaml', 'w', encoding='utf-8') as file:
-                yaml.dump(config, file, default_flow_style=False)
+            config['credentials']['usernames'][username_of_forgotten_password]['pp'] = new_random_password
     except ResetError as e:
         st.error(e)
     except CredentialsError as e:
         st.error(e)
-    st.write('_If you use the password reset widget, please revert the password to what it was before once you are done._')
+    st.write('_If you use the password reset widget please revert the password to what it was before once you are done._')
 
 # Creating a new user registration widget
 try:
     (email_of_registered_user,
      username_of_registered_user,
-     name_of_registered_user) = authenticator.register_user('Register user', preauthorization=False)
+     name_of_registered_user) = authenticator.register_user()
     if email_of_registered_user:
         st.success('User registered successfully')
-        with open('config.yaml', 'w', encoding='utf-8') as file:
-            yaml.dump(config, file, default_flow_style=False)
 except RegisterError as e:
     st.error(e)
 
@@ -88,12 +99,11 @@ except RegisterError as e:
 try:
     (username_of_forgotten_password,
      email_of_forgotten_password,
-     new_random_password) = authenticator.forgot_password('Forgot password')
+     new_random_password) = authenticator.forgot_password()
     if username_of_forgotten_password:
         st.success(f"New password **'{new_random_password}'** to be sent to user securely")
-        config['credentials']['usernames'][username_of_forgotten_password]['password'] = stauth.Hasher([new_random_password]).generate()[0]
-        with open('config.yaml', 'w', encoding='utf-8') as file:
-            yaml.dump(config, file, default_flow_style=False)
+        config['credentials']['usernames'][username_of_forgotten_password]['pp'] = new_random_password
+        # Random password to be transferred to the user securely
     elif not username_of_forgotten_password:
         st.error('Username not found')
 except ForgotError as e:
@@ -102,20 +112,23 @@ except ForgotError as e:
 # Creating a forgot username widget
 try:
     (username_of_forgotten_username,
-     email_of_forgotten_username) = authenticator.forgot_username('Forgot username')
+     email_of_forgotten_username) = authenticator.forgot_username()
     if username_of_forgotten_username:
         st.success(f"Username **'{username_of_forgotten_username}'** to be sent to user securely")
+        # Username to be transferred to the user securely
     elif not username_of_forgotten_username:
         st.error('Email not found')
 except ForgotError as e:
     st.error(e)
 
 # Creating an update user details widget
-if st.session_state.get("authentication_status"):
+if st.session_state["authentication_status"]:
     try:
-        if authenticator.update_user_details(username):
+        if authenticator.update_user_details(st.session_state["username"]):
             st.success('Entries updated successfully')
-            with open('config.yaml', 'w', encoding='utf-8') as file:
-                yaml.dump(config, file, default_flow_style=False)
     except UpdateError as e:
         st.error(e)
+
+# Saving config file
+with open('config.yaml', 'w', encoding='utf-8') as file:
+    yaml.dump(config, file, default_flow_style=False)
